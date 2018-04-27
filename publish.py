7026761f8873dffe940adb696efa369685da9cce
wxdut <wxduter@gmail.com>
2018-04-27 20:56:27 +0800

    some small changes

--- publish.py
+++ publish.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python
 # coding:utf-8
 import os
-
 import re
+
+watermark = "?imageView2/0/interlace/1/q/75|watermark/2/text/d3hkdXQuY29t/font/5b6u6L2v6ZuF6buR/fontsize/320/fill/IzAwMDAwMA==/dissolve/100/gravity/SouthEast/dx/10/dy/10|imageslim"
 
 path = "/Users/hi/Library/Containers/com.coderforart.MWeb/Data/Documents/themes/Site/个人博客"
 os.chdir(path)
@@ -12,10 +13,11 @@ for file in os.listdir("./"):
             s = f.read()
             f.close()
         with open(file, "w") as f:
-            s = re.sub(r'<img src=\"(media/[^\"]+)', r'<img src="https://cdn.wxdut.com/\1', s)
-            s = re.sub(r'<script src=\"asset/chart/all-min.js\"></script>',
-                       r'<script src="https://cdn.wxdut.com/asset/chart/all-min.js"></script>', s)
+            s = re.sub(r'<img src=\"(media/[^\"]+)', r'<img src="https://cdn.wxdut.com/\1' + watermark,
+                       s)  # 把图片替换成CDN资源
+            s = re.sub(r'<link(.*?)href="asset/', r'<link\1href="https://cdn.wxdut.com/asset/', s)  # 把CSS替换成CDN资源
+            s = re.sub(r'<script(.*?)src="asset/', r'<script\1src="https://cdn.wxdut.com/asset/', s)  # 把JS替换成CDN资源
             f.write(s)
             f.close()
 
-print "替换成功"
+print "替换成功 图片、CSS、JS已全部替换成CDN资源。"
