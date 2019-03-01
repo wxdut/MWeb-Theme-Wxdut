e77d6afa66de466cd64c754f66f44f954d20ce8b
hiwang <hiwang@tencent.com>
2019-03-01 15:55:39 +0800

    打开内推

--- asset/js/tencent.js
+++ asset/js/tencent.js
@@ -1,10 +1,11 @@
     $(document).ready(function() {
-        $('#closeTencentModal').modal('show');
-        $("#closeTencentModal").on('hide.bs.modal', function(e) {
-            window.location.href = "https://wxdut.com";
-            return false;
-        });
-        return;
+        // 关闭内推
+        // $('#closeTencentModal').modal('show');
+        // $("#closeTencentModal").on('hide.bs.modal', function(e) {
+        //     window.location.href = "https://wxdut.com";
+        //     return false;
+        // });
+        // return;
         $('#confirm-commit-btn').click(function(event) {
             $('#modal').modal('hide');
             window.location.href = "https://wxdut.com";
