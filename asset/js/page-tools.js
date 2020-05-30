f1a5a8a0712fe4fdfbe24e8f9766472c18d9eaa3
hiwang <hiwang@tencent.com>
2020-05-30 11:27:49 +0800

    时间戳转换

---
+++ asset/js/page-tools.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+	$('#timestampInput').on('input',function(e){
+    	this.value=this.value.replace(/[^\d]/g,'')
+    	refresh()
+	});
+
+	$('#timestampSelect').on('change',function(e){
+    	refresh()
+	});
+
+});
+
+function refresh(argument) {
+	$('#timestampOutput').val(calcuate(parseInt($('#timestampInput').val()) * parseInt($('#timestampSelect').val())))
+}
+
+function calcuate(time = +new Date()) {
+    var date = new Date(time + 8 * 3600 * 1000); // 增加8小时
+    if (isNaN(date.getTime())) {
+    	return "Invalid Timestamp"
+    }
+    return date.toJSON().substr(0, 19).replace('T', ' ');
+}
